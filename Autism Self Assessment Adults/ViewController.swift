07db53bdae9e85a4f2db00ab97b2bc6371c30d4e
jonsalkin <jon.salkin@gmail.com>
2023-03-03 14:58:30 -0500

    Initial Commit

---
+++ Autism Self Assessment Adults/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autism Self Assessment Adults
+//
+//  Created by Jon Salkin on 3/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
